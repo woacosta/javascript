093ac72309505cfdd322cfd30edd79440ac6fe48
woacosta <williamacosta5@gmail.com>
2020-04-16 17:19:16 -0400

    added vars

--- 2-JS-basics/script.js
+++ 2-JS-basics/script.js
@@ -1,2 +1,26 @@
 //this will print to the console log
-console.log('Hello world, again!')
+//console.log('Hello world, again!')
+
+var firstName = 'John';
+console.log(firstName);
+
+var lastName = 'smith';
+var age = 28;
+console.log(age);
+
+var fullAge = true;
+console.log(fullAge);
+
+/* best practice to give meaningful names and to use camelCase
+vars can't start with numbers or symbols except $ or _
+can't use reserved js keywords'*/
+
+var _3years = 3
+
+//undefined datatype
+var job;
+console.log(job);
+
+// now we define the previously undefined
+job = 'teacher';
+console.log(job);
