cfe8026697f0a543d588a1539ecce94500648ac0
woacosta <williamacosta5@gmail.com>
2020-04-17 18:22:02 -0400

    added new function

--- 2-JS-basics/script.js
+++ 2-JS-basics/script.js
@@ -1,3 +1,9 @@
+/*******************************
+* Variables and Data Types
+*/
+
+/*
+
 //this will print to the console log
 //console.log('Hello world, again!')
 
@@ -11,9 +17,9 @@ console.log(age);
 var fullAge = true;
 console.log(fullAge);
 
-/* best practice to give meaningful names and to use camelCase
-vars can't start with numbers or symbols except $ or _
-can't use reserved js keywords'*/
+// best practice to give meaningful names and to use camelCase
+//vars can't start with numbers or symbols except $ or _
+//can't use reserved js keywords'
 
 var _3years = 3
 
@@ -24,3 +30,35 @@ console.log(job);
 // now we define the previously undefined
 job = 'teacher';
 console.log(job);
+
+*/
+
+/*******************************
+* Variable mutation and type coercion
+*/
+
+var firstName = 'John';
+var age = 28;
+
+console.log(firstName + ' ' + age);
+
+// age is automatically converted to a string ie type coercion
+
+// declare many variables in one line
+var job, isMarried;
+job = 'teacher';
+isMarried = false;
+
+console.log(firstName + 'is a ' + age + ' year old ' + job + '. Is he married? ' + isMarried);
+
+
+// Variable mutation - can change var and types on the fly
+age = 'twenty eight';
+job = 'driver';
+
+
+alert(firstName + 'is a ' + age + ' year old ' + job + '. Is he married? ' + isMarried);
+
+var lastName = prompt('what is his last name?');
+console.log(firstName + ' ' + lastName);
+
