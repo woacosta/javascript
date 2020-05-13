bc076efa6d61d9ff81c1d4a822d48b1e198e32ee
woacosta <williamacosta5@gmail.com>
2020-05-13 17:21:48 -0400

    new functions

--- 5-advanced-JS/script.js
+++ 5-advanced-JS/script.js
@@ -64,4 +64,101 @@ var jane = Object.create(personProto,
 */
 
 // Primatives vs objects
+/* for objects, a variable declared in an object doesn't actually hold a copy of the object, it just points to the place in memory where the object is stored. Variables do hold the data they reference.*/
+/*
+//primatives
+var a = 23;
+var b = a;
+a = 46;
+console.log(a);
+console.log(b);
 
+//objects
+var obj1 = {
+    name: 'John',
+    age: 26
+};
+
+var obj2 = obj1;
+obj1.age = 30;
+console.log(obj1.age);
+console.log(obj2.age);
+    
+//functions
+var age =27;
+var obj = {
+    name: 'jonas',
+    city: 'lisbon'
+};
+
+function change(a, b) {
+    a = 30;
+    b.city = 'san francisco'
+}
+
+change(age, obj);
+
+console.log(age);
+console.log(obj.city);
+*/
+
+//*********Passing functions as arguments***********//
+/*
+var years = [1990, 1965, 1937, 2005, 1998];
+
+function arrayCalc(arr, fn) {
+    var arrRes = [];
+    for (var i = 0; i < arr.length; i++) {
+        arrRes.push(fn(arr[i]));
+    }
+    return arrRes;
+}
+
+function calcAge(el) {
+    return 2016 - el;
+}
+
+function isFullAge(el) {
+    return el >= 18;
+}
+
+function maxHeartRate(el) {
+    if (el >= 18 && el <=81) {
+        return Math.round(206.9 - (0.67 * el));
+    } else {
+        return -1;
+    }
+}
+
+var ages = arrayCalc(years, calcAge);
+console.log(ages);
+
+var fullAges = arrayCalc(ages, isFullAge);
+console.log(fullAges);
+
+var rates = arrayCalc(ages, maxHeartRate);
+console.log(rates);
+*/
+
+//*********Functions returning functions***********//
+
+function interviewQuestion(job) {
+    if (job === 'designer') {
+        return function(name) {
+            console.log(name + ', can you explain what UX design is?');
+        }
+    } else if (job === 'teacher') {
+        return function(name) {
+            console.log(name + ', what subject do you teach?');
+        }
+    } else {
+        return function(name) {
+            console.log('hello, ' + name + ', what do you do?');
+        } 
+    }
+}
+
+var teacherQ = interviewQuestion('teacher');
+teacherQ('John');
+
+interviewQuestion('teacher')('mark');
